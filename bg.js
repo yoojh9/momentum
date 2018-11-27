531708074ef1391dcd79f1ace54271798fd58c0b
JeonghyunYoo <yoojh9@hancomm.com>
2018-11-27 13:21:48 +0900

    add background image

---
+++ bg.js
@@ -0,0 +1,27 @@
+const body = document.querySelector('body');
+
+const IMG_NUMBER = 6;
+
+function handleImgLoad(){
+  console.log("finised loaded");
+}
+
+function paintImage(imgNumber){
+  const image = new Image();
+  image.src=`images/${imgNumber+1}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
+  image.addEventListener("loaded", handleImgLoad)
+}
+
+function getRandom(){
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init(){
+  const randomNumber = getRandom();
+  paintImage(randomNumber);
+}
+
+init();
